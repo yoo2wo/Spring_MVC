c9cc51ac154d779ed8d8300cec353cb16e2ef7b2
yooja <wogud758787@naver.com>
2022-01-29 00:07:36 +0900

    springmvc

---
+++ src/main/java/hello/springmvc/basic/LogTestController.java
@@ -0,0 +1,21 @@
+package hello.springmvc.basic;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LogTestController {
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @GetMapping("/log-test")
+    public String logTest() {
+        String name = "Spring";
+
+        System.out.println("name = " + name);
+        log.info("info log={}",name);
+
+        return "ok";
+    }
+}
